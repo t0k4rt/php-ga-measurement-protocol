e21567f3c2936aaee95fcde5054d1ed4e0a6b3e3
Alexandre Assouad <alexandre.assouad@gmail.com>
2014-04-07 11:37:24 +0200

    changed parameters to "GET" instead of "POST"

--- src/Krizon/Google/Analytics/MeasurementProtocol/Resources/service.php
+++ src/Krizon/Google/Analytics/MeasurementProtocol/Resources/service.php
@@ -4,26 +4,21 @@
  * This file is part of the php-ga-measurement-protocol package.
  *
  * (c) Kristian Zondervan <kristian.zondervan@gmail.com>
- * (c) Alexandre Assouad <alexandre.assouad@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * for more information, refer to google documentation :
- * https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
- *
  */
 
 return array(
     'name'       => 'Google Analytics Measurement Protocol PHP Client',
     'operations'            => array(
         'abstract.collect' => array(
-            'httpMethod' => 'POST',
+            'httpMethod' => 'GET',
             'uri'       => 'collect',
             'summary'   => 'Collect data',
             'parameters' => array(
                 'v' => array(
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default'=> 1,
                     'static' => true
                 ),
@@ -32,149 +27,60 @@ return array(
                     'required' => true,
                 ),
                 'cid' => array(
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
-                /** Content information **/
-                'dl' => array(
-                    'description' => 'Document location url',
-                    'location' => 'postField',
-                ),
-                'dh' => array(
-                    'description' => 'Document hostname',
-                    'location' => 'postField',
-                ),
-                'dp' => array(
-                    'description' => 'Document path',
-                    'location' => 'postField',
-                ),
-                'dt' => array(
-                    'description' => 'Document title',
-                    'location' => 'postField',
-                ),
-                'cd' => array(
-                    'description' => 'Content description',
-                    'location' => 'postField',
-                ),
-                'linkid' => array(
-                    'description' => 'The ID of a clicked DOM element',
-                    'location' => 'postField',
-                ),
-                /** Session **/
-                'sc' => array(
-                    'location' => 'postField',
-                    'description' => 'Session control',
-                ),
-                /** Traffic Sources **/
-                'dr' => array(
-                    'location' => 'postField',
-                    'description' => 'Document referrer',
-                ),
-                'cn' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign name',
-                ),
-                'cr' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign source',
-                ),
-                'cm' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign medium',
-                ),
-                'ck' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign keyword',
-                ),
-                'cc' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign content',
-                ),
-                'ci' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the campaign ID',
-                ),
-                'gclid' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the Google AdWords Id',
-                ),
-                'dclid' => array(
-                    'location' => 'postField',
-                    'description' => 'Specifies the Google Display Ads Id',
-                ),
-                /** Application tracking **/
-                'an' => array(
-                    'location' => 'postField',
-                    'description' => 'Application name',
-                ),
-                'av' => array(
-                    'description' => 'Application version',
-                    'location' => 'postField'
-                ),
-                /** Content Experiments **/
-                'xid' => array(
-                    'location' => 'postField',
-                    'description' => 'Experiment ID',
-                ),
-                'xvar' => array(
-                    'description' => 'Experiment variant',
-                    'location' => 'postField'
-                ),
-                /** unofficial **/
                 'ua' => array(
-                    'location' => 'postField',
-                    'description' => 'User-agent override',
-                ),
-                'uip' => array(
-                    'description' => 'ip override',
-                    'location' => 'postField'
+                    'location' => 'header',
+                    'sentAs' => 'User-Agent',
+                    'default' => 'PHP GA Measurement Protocol'
                 ),
                 'cm1' => array(
                     'description' => 'Custom metric 1',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
-                'cm2' => array('location' => 'postField'),
-                'cm3' => array('location' => 'postField'),
-                'cm4' => array('location' => 'postField'),
-                'cm5' => array('location' => 'postField'),
-                'cm6' => array('location' => 'postField'),
-                'cm7' => array('location' => 'postField'),
-                'cm8' => array('location' => 'postField'),
-                'cm9' => array('location' => 'postField'),
-                'cm10' => array('location' => 'postField'),
-                'cm11' => array('location' => 'postField'),
-                'cm12' => array('location' => 'postField'),
-                'cm13' => array('location' => 'postField'),
-                'cm14' => array('location' => 'postField'),
-                'cm15' => array('location' => 'postField'),
-                'cm16' => array('location' => 'postField'),
-                'cm17' => array('location' => 'postField'),
-                'cm18' => array('location' => 'postField'),
-                'cm19' => array('location' => 'postField'),
-                'cm20' => array('location' => 'postField'),
+                'cm2' => array('location' => 'query'),
+                'cm3' => array('location' => 'query'),
+                'cm4' => array('location' => 'query'),
+                'cm5' => array('location' => 'query'),
+                'cm6' => array('location' => 'query'),
+                'cm7' => array('location' => 'query'),
+                'cm8' => array('location' => 'query'),
+                'cm9' => array('location' => 'query'),
+                'cm10' => array('location' => 'query'),
+                'cm11' => array('location' => 'query'),
+                'cm12' => array('location' => 'query'),
+                'cm13' => array('location' => 'query'),
+                'cm14' => array('location' => 'query'),
+                'cm15' => array('location' => 'query'),
+                'cm16' => array('location' => 'query'),
+                'cm17' => array('location' => 'query'),
+                'cm18' => array('location' => 'query'),
+                'cm19' => array('location' => 'query'),
+                'cm20' => array('location' => 'query'),
                 'cd1' => array(
                     'description' => 'Custom dimension 1',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
-                'cd2' => array('location' => 'postField'),
-                'cd3' => array('location' => 'postField'),
-                'cd4' => array('location' => 'postField'),
-                'cd5' => array('location' => 'postField'),
-                'cd6' => array('location' => 'postField'),
-                'cd7' => array('location' => 'postField'),
-                'cd8' => array('location' => 'postField'),
-                'cd9' => array('location' => 'postField'),
-                'cd10' => array('location' => 'postField'),
-                'cd11' => array('location' => 'postField'),
-                'cd12' => array('location' => 'postField'),
-                'cd13' => array('location' => 'postField'),
-                'cd14' => array('location' => 'postField'),
-                'cd15' => array('location' => 'postField'),
-                'cd16' => array('location' => 'postField'),
-                'cd17' => array('location' => 'postField'),
-                'cd18' => array('location' => 'postField'),
-                'cd19' => array('location' => 'postField'),
-                'cd20' => array('location' => 'postField'),
+                'cd2' => array('location' => 'query'),
+                'cd3' => array('location' => 'query'),
+                'cd4' => array('location' => 'query'),
+                'cd5' => array('location' => 'query'),
+                'cd6' => array('location' => 'query'),
+                'cd7' => array('location' => 'query'),
+                'cd8' => array('location' => 'query'),
+                'cd9' => array('location' => 'query'),
+                'cd10' => array('location' => 'query'),
+                'cd11' => array('location' => 'query'),
+                'cd12' => array('location' => 'query'),
+                'cd13' => array('location' => 'query'),
+                'cd14' => array('location' => 'query'),
+                'cd15' => array('location' => 'query'),
+                'cd16' => array('location' => 'query'),
+                'cd17' => array('location' => 'query'),
+                'cd18' => array('location' => 'query'),
+                'cd19' => array('location' => 'query'),
+                'cd20' => array('location' => 'query'),
             )
         ),
         'pageview' => array(
@@ -182,10 +88,22 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Pageview hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'pageview',
                     'static' => true
-                )
+                ),
+                'dh' => array(
+                    'description' => 'Document hostname',
+                    'location' => 'query',
+                ),
+                'dp' => array(
+                    'description' => 'Page',
+                    'location' => 'query',
+                ),
+                'dt' => array(
+                    'description' => 'Title',
+                    'location' => 'query',
+                ),
             )
         ),
         'event' => array(
@@ -193,25 +111,25 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Event hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'event',
                     'static' => true
                 ),
                 'ec' => array(
                     'description' => 'Event category',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'ea' => array(
                     'description' => 'Event action',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'el' => array(
                     'description' => 'Event label',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'ev' => array(
                     'description' => 'Event value',
-                    'location' => 'postField',
+                    'location' => 'query',
                 )
             )
         ),
@@ -220,34 +138,34 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Transaction hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'transaction',
                     'static' => true
                 ),
                 'ti' => array(
                     'description' => 'transaction ID',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
                 'ta' => array(
                     'description' => 'Transaction affiliation',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'tr' => array(
                     'description' => 'Transaction revenue',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'ts' => array(
                     'description' => 'Transaction shipping',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'tt' => array(
                     'description' => 'Transaction tax',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'cu' => array(
                     'description' => 'Currency code',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
             )
         ),
@@ -256,39 +174,39 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Item hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'item',
                     'static' => true
                 ),
                 'ti' => array(
                     'description' => 'transaction ID',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
                 'in' => array(
                     'description' => 'Item name',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
                 'ip' => array(
                     'description' => 'Item price',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'iq' => array(
                     'description' => 'Item quantity',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'ic' => array(
                     'description' => 'Item code / SKU',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'iv' => array(
                     'description' => 'Item variation / category',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
                 'cu' => array(
                     'description' => 'Currency code',
-                    'location' => 'postField',
+                    'location' => 'query',
                 ),
             )
         ),
@@ -297,23 +215,23 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Social hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'social',
                     'static' => true
                 ),
                 'sa' => array(
                     'description' => 'Social Action',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
                 'sn' => array(
                     'description' => 'Social Network',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
                 'st' => array(
                     'description' => 'Social Target',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'required' => true
                 ),
             )
@@ -323,17 +241,17 @@ return array(
             'parameters' => array(
                 't' => array(
                     'description' => 'Exception hit type',
-                    'location' => 'postField',
+                    'location' => 'query',
                     'default' => 'exception',
                     'static' => true
                 ),
                 'exd' => array(
                     'description' => 'Exception description',
-                    'location' => 'postField'
+                    'location' => 'query'
                 ),
                 'exf' => array(
                     'description' => 'Exception is fatal?',
-                    'location' => 'postField'
+                    'location' => 'query'
                 )
             )
         )
